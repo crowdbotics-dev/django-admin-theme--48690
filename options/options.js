0d15793f564a5a330de621054b1ef52d772452bf
Crowdbotics <engineering@crowdbotics.com>
2022-12-03 22:01:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "django-admin-theme--48690",
+  url: "https://django-admin-theme--48690.botics.co",
+  api: "https://django-admin-theme--48690.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
